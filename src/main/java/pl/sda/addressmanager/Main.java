4edc80aa15b08ec799a2b4c6b63831819a732f95
Damian Kolczyński <damian.kolczynski@gmail.com>
2019-02-16 10:27:23 +0100

    Init Repo

---
+++ src/main/java/pl/sda/addressmanager/Main.java
@@ -0,0 +1,21 @@
+package pl.sda.addressmanager;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/VerticalBox.fxml"));
+        primaryStage.setTitle("Weather Service");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/pl/sda/addressmanager/controller/VerticalBoxController.java
@@ -0,0 +1,12 @@
+package pl.sda.addressmanager.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VerticalBoxController implements Initializable {
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
